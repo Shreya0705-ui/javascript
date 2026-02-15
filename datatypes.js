7006b1c90b3cccfd2406e8bc3a4811454b2a5007
Shreya0705-ui <gangwarshreya0705@gmail.com>
2026-02-15 18:38:32 +0530

    Datatypes and conversion operations

---
+++ datatypes.js
@@ -0,0 +1,69 @@
+let name = "Shreya"
+let age = 21
+let isLoggedIn = false;
+
+
+
+
+// There are many datatypes :
+
+//  number : 2 to power of 53
+// bigInt
+// string : ""
+// boolean : true/false
+// null : standalone value
+// undefined : value not assigned
+//  symbol : unique 
+
+//  object
+
+
+
+
+
+
+console.log(typeof age)
+
+console.log(typeof isLoggedIn)
+
+
+
+
+
+// conversion 
+
+
+
+let score = "33aabv"
+
+console.log(typeof score );
+
+let valueInNumber = Number (score)
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+
+
+
+let islogged = ""
+
+
+console.log(typeof islogged);
+
+let booleanislggedIn = Boolean(islogged)
+console.log(booleanislggedIn);
+
+
+
+
+
+let someNumber = 33
+
+console.log(someNumber)
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
